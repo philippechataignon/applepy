5a8403ac27008e047b88f789f25d5d39b4af42f8
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-01-16 11:44:25 +0100

    Load hex file instead of bin

--- applepy.py
+++ applepy.py
@@ -3,6 +3,7 @@
 # ApplePy - an Apple ][ emulator in Python
 # James Tauber / http://jtauber.com/
 # originally written 2001, updated 2011
+# updated 2021 Philippe Chataignon
 
 import pygame
 import sys
@@ -15,9 +16,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--rom", type=str, default="apple2plus.rom", help="ROM image file")
-    parser.add_argument("--load", type=str, help="program  image file")
-    parser.add_argument("--address", type=int, default=0x1000, help="load address")
-    parser.add_argument("--rom_address", type=int, default=0xd000, help="ROM load address")
+    parser.add_argument("--load", type=str, help="program hex file")
     parser.add_argument("--log", type=str, help="log file")
     options = parser.parse_args()
     display = Display()
