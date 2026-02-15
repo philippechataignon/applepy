c22e908dde4f50b36286af8652b10112c8be4dec
Philippe Chataignon <philippe.chataignon@gmail.com>
2026-02-15 17:34:07 +0100

    Put signed function in cpu

--- cpu.py
+++ cpu.py
@@ -1,5 +1,9 @@
 import pygame
-from utils import signed
+
+def signed(x):
+    if x > 0x7F:
+        x = x - 0x100
+    return x
 
 class CPU:
 
--- utils.py
+++
@@ -1,4 +0,0 @@
-def signed(x):
-    if x > 0x7F:
-        x = x - 0x100
-    return x
